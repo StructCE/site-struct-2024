e63a4dafde5d5e56107b925b2bda40b25d5b72fe
leopcortes <leo.pereira.cortes@gmail.com>
2024-03-04 00:01:59 -0300

    home section

--- src/components/PageComponents/Homepage/Home/page.tsx
+++ src/components/PageComponents/Homepage/Home/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <>
-      <div className="z-2 m-0 flex h-[650px] w-full items-center justify-center p-0 lg:h-[1000px]">
+      <section className="m-0 flex h-[650px] w-full items-center justify-center p-0 lg:h-[1000px]">
         <div className="w-[280px] sm:w-[600px] lg:w-[750px]">
           <h1 className="text-center font-oxanium text-[72px] font-bold leading-[90px] sm:text-[156px] sm:leading-[195px] lg:text-[192px] lg:leading-[240px]">
             {"{struct}"}
@@ -12,7 +12,7 @@ export default function Home() {
             Atuando no mercado de desenvolvimento web desde 2014.
           </p>
         </div>
-      </div>
+      </section>
     </>
   );
 }
