94afdb1f1a319273724a6344d66ec657ae8ff1ea
MatheusNevs <matheusnf62@gmail.com>
2024-03-08 16:09:54 -0300

    primary router updated

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,8 @@
 import { projectRouter } from "~/server/api/routers/project";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { partnerRouter } from "./routers/partner";
+import { directorshipRouter } from "./routers/directorship";
+import { memberRouter } from "./routers/member";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +11,9 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   project: projectRouter,
+  partner: partnerRouter,
+  directorship: directorshipRouter,
+  member: memberRouter
 });
 
 // export type definition of API
