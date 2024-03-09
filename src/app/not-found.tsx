13f3edb29bb39353fc371be1e9d041d9429650df
leopcortes <leo.pereira.cortes@gmail.com>
2024-03-09 16:15:36 -0300

    cor do fundo hover

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="bg-fundo-0 text-struct-7 flex h-screen w-full flex-col items-center justify-center gap-4 font-oxanium font-bold">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-fundo-0 font-oxanium font-bold text-struct-7">
       <h2 className="text-6xl">404 | Not Found</h2>
       <p className="font-nunito text-xl font-normal">
         A página que você está procurando não existe
       </p>
       <Link
         href="/"
-        className="bg-struct-7 text-struct-1 m-2 rounded-lg px-6 py-2 text-xl hover:bg-[#ebebeb]"
+        className="hover:bg-struct-7-hover m-2 rounded-lg bg-struct-7 px-6 py-2 text-xl text-struct-1"
       >
         Voltar
       </Link>
