cda60281004b1ff0b7a2d052fd2bccde4e971cd8
leopcortes <leo.pereira.cortes@gmail.com>
2024-03-09 16:16:00 -0300

    parceiros - aboutus section

---
+++ src/components/PageComponents/Homepage/AboutUs/PartnerCard/page.tsx
@@ -0,0 +1,32 @@
+import { CldImage } from "next-cloudinary";
+import Link from "next/link";
+
+type Image = {
+  url: string;
+  alt: string;
+  link: string;
+};
+
+type Props = {
+  images: Image;
+};
+
+export default function PartnerCard({ images }: Props) {
+  return (
+    <>
+      <Link
+        href={images.link}
+        target="_blank"
+        className="hover:bg-struct-7-hover size-28 rounded-full bg-struct-7"
+      >
+        <CldImage
+          alt={images.alt}
+          src={images.url}
+          width={120}
+          height={120}
+          className="rounded-full"
+        ></CldImage>
+      </Link>
+    </>
+  );
+}
--- src/components/PageComponents/Homepage/AboutUs/page.tsx
+++ src/components/PageComponents/Homepage/AboutUs/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
-// import { api } from "~/trpc/server";
+import { partnerRouter } from "~/server/api/routers/partner";
+import { api } from "~/trpc/react";
+import PartnerCard from "./PartnerCard/page";
 
-export default async function Home() {
-  //   const partners = await api.partner.getAll.query();
+export default function Home() {
+  const { data } = api.partner.getAll.useQuery();
+
+  console.log(data);
 
   return (
     <>
@@ -13,9 +18,10 @@ export default async function Home() {
             <span className="text-struct-3">{"{ "}</span>Quem Somos
             <span className="text-struct-3">{" }"}</span>
           </h2>
-          <div className="bg-fundo-3 flex w-[1024px] rounded-2xl">
+          <div className="flex w-[1024px] rounded-2xl bg-fundo-3">
             {/* Texto e números */}
             <div className="m-8 mr-4 w-[523px]">
+              {/* Texto */}
               <p className="mb-8">
                 A Struct é a Empresa Junior (EJ) do curso Engenharia de
                 Computação da Universidade de Brasília. Desde 2014, buscamos
@@ -27,21 +33,22 @@ export default async function Home() {
                 universitários, com o objetivo de ter um ambiente que provê uma
                 vivência empresarial aos estudantes.
               </p>
+              {/* Números */}
               <div className="flex justify-center gap-8 text-[24px] font-normal">
-                <p className="flex w-[120px] flex-col text-center">
-                  <span className="text-struct-1 font-oxanium text-[96px] font-semibold leading-[90px]">
+                <p className="flex w-[130px] flex-col text-center">
+                  <span className="font-oxanium text-[96px] font-semibold leading-[90px] text-struct-1">
                     55
                   </span>{" "}
                   projetos concluídos
                 </p>
-                <p className="flex w-[120px] flex-col text-center">
-                  <span className="text-struct-1 font-oxanium text-[96px] font-semibold leading-[90px]">
+                <p className="flex w-[130px] flex-col text-center">
+                  <span className="font-oxanium text-[96px] font-semibold leading-[90px] text-struct-1">
                     9
                   </span>{" "}
                   anos de atividade
                 </p>
-                <p className="flex w-[120px] flex-col text-center">
-                  <span className="text-struct-1 font-oxanium text-[96px] font-semibold leading-[90px]">
+                <p className="flex w-[130px] flex-col text-center">
+                  <span className="font-oxanium text-[96px] font-semibold leading-[90px] text-struct-1">
                     27
                   </span>{" "}
                   membros ativos
@@ -51,7 +58,7 @@ export default async function Home() {
             {/* MVV */}
             <div className="m-8 ml-4 w-[405px] rounded-lg px-8 shadow-[0_1px_12px_rgb(8,20,38,0.75)]">
               {/* Título */}
-              <h4 className="text-struct-2 mb-2 mt-4 flex justify-center font-oxanium text-[24px] font-bold">
+              <h4 className="mb-2 mt-4 flex justify-center font-oxanium text-[24px] font-bold text-struct-2">
                 MVV
               </h4>
               {/* Missão, Valores e Visão */}
@@ -84,27 +91,27 @@ export default async function Home() {
                     </h5>
                     <div className="flex flex-col gap-2 p-2 font-bold">
                       <p>
-                        <span className="text-struct-2 font-oxanium">S</span>
+                        <span className="font-oxanium text-struct-2">S</span>
                         inergia
                       </p>
                       <p>
-                        <span className="text-struct-2 font-oxanium">T</span>
+                        <span className="font-oxanium text-struct-2">T</span>
                         ransparência
                       </p>
                       <p>
-                        <span className="text-struct-2 font-oxanium">R</span>
+                        <span className="font-oxanium text-struct-2">R</span>
                         espeito
                       </p>
                       <p>
-                        <span className="text-struct-2 font-oxanium">U</span>
+                        <span className="font-oxanium text-struct-2">U</span>
                         nião
                       </p>
                       <p>
-                        <span className="text-struct-2 font-oxanium">C</span>
+                        <span className="font-oxanium text-struct-2">C</span>
                         ompromisso
                       </p>
                       <p>
-                        <span className="text-struct-2 font-oxanium">T</span>
+                        <span className="font-oxanium text-struct-2">T</span>
                         ransformadora
                       </p>
                     </div>
@@ -115,7 +122,7 @@ export default async function Home() {
               <div className="m-2 flex items-center justify-center">
                 <Link
                   href="/membros"
-                  className="text-struct-3 border-struct-3 border-b-2 py-1 font-oxanium text-[24px] font-semibold leading-[20px]"
+                  className="border-b-2 border-struct-3 py-1 font-oxanium text-[24px] font-semibold leading-[20px] text-struct-3"
                 >
                   conheça nossos membros
                 </Link>
@@ -129,7 +136,18 @@ export default async function Home() {
             <span className="text-struct-3">{"{ "}</span>Parceiros
             <span className="text-struct-3">{" }"}</span>
           </h3>
-          <div className="bg-fundo-3"></div>
+          <div className="flex gap-8 rounded-xl bg-fundo-3 p-8">
+            {data?.map((partner) => (
+              <PartnerCard
+                key={partner.id}
+                images={{
+                  url: partner.logoPublicId,
+                  alt: partner.name,
+                  link: partner.link,
+                }}
+              />
+            ))}
+          </div>
         </div>
       </section>
     </>
