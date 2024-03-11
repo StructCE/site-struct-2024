4ecfd55900dc8f04a5c56ac5da5a998ed068429d
MatheusNevs <matheusnf62@gmail.com>
2024-03-11 19:48:26 -0300

    portfolio added to the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-"use client";
 import Home from "../components/PageComponents/Homepage/Home/page";
 import Services from "../components/PageComponents/Homepage/Services/page";
-
+import Portfolio from "../components/PageComponents/Homepage/Portfolio/page";
 export default function Homepage() {
   return (
     <>
       <div className="w-full bg-[#081426] text-[#F8F8FF] selection:bg-[#0A369F]">
         <Home />
         <Services />
-        {/* Portfólio */}
+        <Portfolio />
         {/* Sobre nós */}
         {/* Contato */}
       </div>
