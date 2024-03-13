c396a4b6c345c35ae7909e0cbfc5351fb23a335f
MatheusNevs <matheusnf62@gmail.com>
2024-03-13 18:36:22 -0300

    adjusting a few details and fixing colors

--- src/app/members/memberContainer.tsx
+++ src/app/members/memberContainer.tsx
@@ -7,34 +7,45 @@ type Member = {
   role: string
 }
 
-const getColor = (role: string, component:string): string => {
-  const roleColors: { [key: string]: string } = {
-    "Presidente": "[#FD8D4B]",
-    "Assessor": "[#FD8D4B]",
-    "Diretor": "[#2DA2FF]",
-    "Membro": "white"
-  };
+const borderColors: { [key: string]: string } = {
+  "Presidente": "border-struct-1",
+  "Assessor": "border-struct-1",
+  "Diretor": "border-struct-3",
+  "Membro": "border-struct-7"
+}
 
-  const res = `${component}-${roleColors[role]}`
-  return res;
+const textColors: { [key: string]: string } = {
+  "Presidente": "text-struct-1",
+  "Assessor": "text-struct-1",
+  "Diretor": "text-struct-3",
+  "Membro": "text-struct-7"
+}
+
+const getColor = (role: string, component:string): string => {
+  let res
+  if (component === "text")
+    res = textColors[role]
+  if (component === "border")
+    res = borderColors[role]
+  return res || "" 
 };
 
 export default function MemberContainer({member}: {member: Member}) {
   return (
-    <div className="flex flex-col w-[170px] h-[190px] m-7 items-center" >
+    <div className="flex flex-col w-[170px] h-[190px] m-10 sm:m-8 items-center" >
       <CldImage
-      width="120"
-      height="120"
+      width="150"
+      height="150"
       src={member.logoPublicId}
       alt="Foto de Membro"
       crop="fill"
-      className={"rounded-full bg-white border-[3px] "+ getColor(member.role, "border")}
+      className={"rounded-full bg-struct-7 border-[3px] "+ getColor(member.role, "border")}
       />
       <div className="m-5 w-full h-fit flex flex-col items-center justify-center">
-        <p className="font-nunito text-[16px] text-white text-center">
+        <p className="font-nunito text-[16px] text-struct-7 text-center">
           {member.name}
         </p>
-        <p className={"font-nunito text-[12px] text-center " + getColor(member.role, "text")}>
+        <p className={"font-nunito text-[14px] text-opacity-70 text-center " + getColor(member.role, "text")}>
           {member.role}
         </p>
       </div>
