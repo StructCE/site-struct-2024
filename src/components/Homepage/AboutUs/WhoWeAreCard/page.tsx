1160aa18cf5dfda86cea1226e01d34c5ba8e8e8a
leopcortes <leo.pereira.cortes@gmail.com>
2024-03-17 17:37:10 -0300

    renaming folders

--- src/components/Homepage/AboutUs/WhoWeAreCard/page.tsx
+++ src/components/Homepage/AboutUs/WhoWeAreCard/page.tsx
@@ -117,7 +117,7 @@ export default async function WhoWeArePage() {
         {/* Botão */}
         <div className="m-2 mb-4 flex items-center justify-center">
           <Link
-            href="/members"
+            href="/membros"
             className="border-animation border-b-[1px] border-struct-1 font-oxanium text-[14px] font-semibold leading-[16px] text-struct-3 sm:border-b-2 sm:py-1 sm:text-[24px] sm:leading-[20px]"
           >
             conheça nossos membros
