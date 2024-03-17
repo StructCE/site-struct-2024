06cf008367a2a5782b99133d343626dfae9ca32c
leopcortes <leo.pereira.cortes@gmail.com>
2024-03-17 00:48:02 -0300

    animação home section - homepage

--- src/components/Homepage/Home/page.tsx
+++ src/components/Homepage/Home/page.tsx
@@ -5,24 +5,36 @@ export default function Home() {
   return (
     <>
       <section
-        className="m-0 flex h-[650px] w-full items-center justify-center p-0 lg:h-[1000px]"
+        className="m-0 flex h-[650px] w-full items-center justify-center p-0 lg:h-[1100px]"
         id="home"
       >
         <div className="w-[280px] sm:w-[600px] lg:w-[750px]">
-          <h1 className="text-center font-oxanium text-[72px] font-bold leading-[90px] sm:text-[156px] sm:leading-[195px] lg:text-[192px] lg:leading-[240px]">
-            {"{struct}"}
-          </h1>
           <TypeAnimation
-            sequence={["{struct}", 1000]}
-            speed={10}
-            wrapper="span"
+            sequence={["", 750, "{struct}"]}
+            speed={{ type: "keyStrokeDelayInMs", value: 150 }}
+            wrapper="h1"
             cursor={false}
-            repeat={Infinity}
-            className="ml-2 text-center font-oxanium text-[72px] font-bold leading-[90px] sm:text-[156px] sm:leading-[195px] lg:text-[192px] lg:leading-[240px]"
+            className="text-center font-oxanium text-[72px] font-bold leading-[90px] sm:text-[156px] sm:leading-[195px] lg:text-[192px] lg:leading-[240px]"
           />
-          <p className="text-center font-nunito text-[12px] font-bold sm:text-[20px] lg:text-[24px]">
+          {/* <p className="text-center font-nunito text-[12px] font-bold sm:text-[20px] lg:text-[24px]">
             Atuando no mercado de desenvolvimento web desde 2014.
-          </p>
+          </p> */}
+          {/* <h1 className="text-center font-oxanium text-[72px] font-bold leading-[90px] sm:text-[156px] sm:leading-[195px] lg:text-[192px] lg:leading-[240px]">
+            {"{struct}"}
+          </h1> */}
+          <TypeAnimation
+            sequence={[
+              "",
+              3000,
+              "Atuando no mercado de desenvolvimento web desde 2014.",
+            ]}
+            speed={{ type: "keyStrokeDelayInMs", value: 40 }}
+            wrapper="p"
+            cursor={false}
+            repeat={0}
+            className="text-center font-nunito text-[12px] font-bold sm:text-[20px] lg:text-[24px]"
+          />
+          <p className="text-[1px] leading-[150px] text-fundo-0">.</p>
         </div>
       </section>
     </>
