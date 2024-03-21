76dc8034388cd845718eda518a6146e65cd65040
leopcortes <leo.pereira.cortes@gmail.com>
2024-03-21 19:38:07 -0300

    portfolio styles

--- src/components/Homepage/Portfolio/ProjectsContainer/page.tsx
+++ src/components/Homepage/Portfolio/ProjectsContainer/page.tsx
@@ -17,7 +17,7 @@ export default function ProjectsContainer({
         exit={{ opacity: "var(--opacity-from)", y: "var(--y-to)" }}
         transition={{ ease: "easeInOut", duration: 1, delay: 0 }}
         viewport={{ once: true }}
-        className="flex flex-col items-center justify-center [--opacity-from:1%] [--opacity-to:100%] [--y-from:75px] [--y-to:0px]"
+        className="mb-3 flex flex-col items-center justify-center [--opacity-from:1%] [--opacity-to:100%] [--y-from:75px] [--y-to:0px] sm:mb-4"
       >
         <h2 className="text-center font-oxanium text-[24px] font-bold leading-[30px] sm:text-[32px] sm:leading-[48px] lg:text-[48px] lg:leading-[60px]">
           <span className="text-[#2DA2FF]">{"{ "}</span>Portfólio
@@ -29,31 +29,28 @@ export default function ProjectsContainer({
         </p>
       </motion.div>
       {/* Projetos */}
-      <div className="m-2 items-center justify-center sm:m-4">
-        {projects.map((project, index) => {
-          if (index % 2 === 0) {
-            const nextProject = projects[index + 1];
-            return (
-              <motion.div
-                key={project.id}
-                initial={{
-                  opacity: "var(--opacity-from)",
-                  y: "var(--y-from)",
-                }}
-                whileInView={{
-                  opacity: "var(--opacity-to)",
-                  y: "var(--y-to)",
-                }}
-                exit={{ opacity: "var(--opacity-from)", y: "var(--y-to)" }}
-                transition={{ ease: "easeInOut", duration: 1, delay: 0.2 }}
-                viewport={{ once: true }}
-                className="flex w-full items-center justify-center text-lg [--opacity-from:1%] [--opacity-to:100%] [--y-from:75px] [--y-to:0px] max-lg:flex-col"
-              >
-                <ProjectCard project={project} />
-                {nextProject ? <ProjectCard project={nextProject} /> : null}
-              </motion.div>
-            );
-          }
+      <div className="flex flex-col flex-wrap items-center justify-center lg:w-[1024px] lg:flex-row">
+        {projects.map((project) => {
+          return (
+            <motion.div
+              key={project.id}
+              initial={{
+                opacity: "var(--opacity-from)",
+                y: "var(--y-from)",
+              }}
+              whileInView={{
+                opacity: "var(--opacity-to)",
+                y: "var(--y-to)",
+              }}
+              exit={{ opacity: "var(--opacity-from)", y: "var(--y-to)" }}
+              transition={{ ease: "easeInOut", duration: 1, delay: 0.2 }}
+              viewport={{ once: true }}
+              className="text-lg [--opacity-from:1%] [--opacity-to:100%] [--y-from:75px] [--y-to:0px]"
+            >
+              <ProjectCard project={project} />
+            </motion.div>
+          );
+          // }
         })}
       </div>
     </>
