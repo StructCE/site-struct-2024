f573ec81d52bc3dc39dc717fe295c9092251d93a
leopcortes <leo.pereira.cortes@gmail.com>
2024-03-22 17:09:03 -0300

    cursor type animation

--- src/components/Homepage/Home/page.tsx
+++ src/components/Homepage/Home/page.tsx
@@ -2,6 +2,8 @@
 import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
+  const CURSOR_CLASS_NAME = "custom-type-animation-cursor";
+
   return (
     <>
       <section
@@ -10,23 +12,25 @@ export default function Home() {
       >
         <div className="w-[280px] sm:w-[600px] lg:w-[750px]">
           <TypeAnimation
+            cursor={false}
             sequence={["", 600, "{struct}"]}
             speed={{ type: "keyStrokeDelayInMs", value: 150 }}
             wrapper="h1"
-            cursor={false}
             className="text-center font-oxanium text-[72px] font-bold leading-[90px] sm:text-[156px] sm:leading-[195px] lg:text-[192px] lg:leading-[240px]"
           />
           <TypeAnimation
+            cursor={false}
+            className={CURSOR_CLASS_NAME}
             sequence={[
+              (el) => el!.classList.remove(CURSOR_CLASS_NAME),
               "",
               2550,
+              (el) => el!.classList.add(CURSOR_CLASS_NAME),
               "Atuando no mercado de desenvolvimento web desde 2014.",
             ]}
             speed={{ type: "keyStrokeDelayInMs", value: 40 }}
             wrapper="p"
-            cursor={false}
             repeat={0}
-            className="text-center font-nunito text-[12px] font-bold sm:text-[20px] lg:text-[24px]"
           />
           <p className="text-[1px] leading-[0px] text-fundo-0 lg:leading-[150px]">
             .
