7b64be67e47699e9fe13a9e32a9d07b3e2a19b01
leopcortes <leo.pereira.cortes@gmail.com>
2024-03-24 22:49:44 -0300

    enviromental variables

--- src/components/Homepage/ContactUs/FindUs/page.tsx
+++ src/components/Homepage/ContactUs/FindUs/page.tsx
@@ -25,7 +25,7 @@ export default function FindUs() {
               className="h-[248px] w-[248px] border-none sm:h-[448px] sm:w-[448px]"
               loading="lazy"
               allowFullScreen
-              src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ2eWshbo7WpMRyW_XzFV-Glc&key=${process.env.MAP_API_KEY}`}
+              src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ2eWshbo7WpMRyW_XzFV-Glc&key=${process.env.NEXT_PUBLIC_MAP_API_KEY}`}
             ></iframe>
           </div>
           {/* Redes Sociais */}
