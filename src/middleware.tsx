808ac7c4a9ce9fb8b713f73c414db7ef88f55993
paulo <paulofvilarimda@gmail.com>
2024-07-28 23:35:02 -0300

    fix middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -4,22 +4,30 @@ import { type Session } from "next-auth";
 
 export default async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
+  const token = request.cookies.get("next-auth.session-token");
 
-  const session = (await (
-    await fetch(process.env.NEXTAUTH_URL + "/api/auth/session", {
-      method: "GET",
-      headers: {
-        ...Object.fromEntries(request.headers),
-      },
-    })
-  ).json()) as Session;
-
-  if (pathname.startsWith("/login") && session?.user?.id) {
+  if (pathname.startsWith("/login") && token) {
     return NextResponse.redirect(getUrlByPath("/"));
   }
 
-  if (pathname.startsWith("/dashboard") && !session?.user?.id) {
-    return NextResponse.redirect(getUrlByPath("/login"));
+  if (pathname.startsWith("/dashboard")) {
+    let session;
+
+    if (token) {
+      console.log("rodou o fetch");
+      session = (await (
+        await fetch(process.env.NEXTAUTH_URL + "/api/auth/session", {
+          method: "GET",
+          headers: {
+            ...Object.fromEntries(request.headers),
+          },
+        })
+      ).json()) as Session;
+    }
+
+    return session?.user?.id
+      ? NextResponse.next()
+      : NextResponse.redirect(getUrlByPath("/login"));
   }
 
   return NextResponse.next();
